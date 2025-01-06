18827371912103715d94a6cd2eda1b7f9e0720f2
Mohd Tauseef Ansari <mohdtauseefansari34@gmail.com>
2025-01-06 21:10:43 +0530

    fixing game

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-// Adding default img into the game
-
-let img1 = (document.getElementById("img1").src = "/images/dice6.png");
-let img2 = (document.getElementById("img2").src = "/images/dice6.png");
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber1);
-console.log(randomNumber2);
 
-document.getElementById("img1").src = `/images/dice${randomNumber1}.png`;
-document.getElementById("img2").src = `/images/dice${randomNumber2}.png`;
+document.getElementById("img1").src = `images/dice${randomNumber1}.png`;
+document.getElementById("img2").src = `images/dice${randomNumber2}.png`;
 
 if (randomNumber1 > randomNumber2) {
   document.getElementById("heading").innerHTML = `🚩Player 1 wins!`;
